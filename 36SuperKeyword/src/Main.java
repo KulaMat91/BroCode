bdd73d6b21d0b3c2281bc067ab167927a9e301dc
Mateusz Kulinski <kula.mat91@gmail.com>
2022-10-21 01:06:45 +0200

    Commit night

--- 36SuperKeyword/src/Main.java
+++ 36SuperKeyword/src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Hero hero1 = new Hero("Batman", 42, "$$$");
 
+        System.out.println(hero1.name);
+        System.out.println(hero1.age);
+        System.out.println(hero1.power);
+
+        Hero hero2 = new Hero("Superman", 43, "Power");
+
+        System.out.println(hero2.toString());
     }
 }
---
+++ 37Abstract/src/Vehicle.java
@@ -0,0 +1,3 @@
+public abstract class Vehicle {
+    abstract void go(); // you don't have to have body for this method
+}
