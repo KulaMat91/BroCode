ba1c43541703647153b97de082fce2b667afc153
Mateusz Kulinski <kula.mat91@gmail.com>
2022-11-01 23:51:50 +0100

    47

--- 46FileWriter/src/Main.java
+++ 46FileWriter/src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            FileWriter writer = new FileWriter("C:\\KURS JAVA\\JAVA\\BroCode\\46FileWriter\\src\\poem.txt");
+            writer.write("Roses are red\nViolets are blue \nBooty booty booty \nRocking everywhere");
+            writer.append("\nPoem");
+            writer.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
