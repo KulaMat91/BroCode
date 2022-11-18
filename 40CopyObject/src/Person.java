0bf214913407ffa2e72c3b707e423046bae0a289
Mateusz Kuliński <kula.mat91@gmail.com>
2022-11-18 23:51:53 +0100

    40 commit'
    '

---
+++ 40CopyObject/src/Person.java
@@ -0,0 +1,41 @@
+public class Person {
+    private String name;
+    private String surname;
+    private Integer age;
+
+    public Person(String name, String surname, Integer age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Person(Person person){
+        this.name = getName();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+
+}
